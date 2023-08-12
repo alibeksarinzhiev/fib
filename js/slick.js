4d29ec97c7451cd7d3ca958f539ce5a0bbb6ea5c
intend1000 <intend1000@gmail.com>
2023-08-12 10:32:12 +0600

    cart edit

---
+++ js/slick.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function () {
+  $('.pizza-slick').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    dots:true,
+    speed:3000,
+    slidesPerRow:1
+  });
+});
